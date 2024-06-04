6a96275b546f5c05af36ec83a22030ed1192e589
millyanne93 <nmillyanne20@gmail.com>
2024-06-04 08:03:28 +0000

    auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,14 +1,29 @@
+#!/usr/bin/env python3
+""" API authentication
+"""
 from flask import request
+from typing import List, TypeVar
 
-class Auth:
+
+class Auth():
+    """ manages the API authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        # Placeholder logic for future implementation
-        return False
+        """ require authorithation check"""
+        if path is None or excluded_paths is None or not len(excluded_paths):
+            return True
+        if path[-1] != '/':
+            path += '/'
+        for p in excluded_paths:
+            if p.endswith('*'):
+                if path.startswith(p[:1]):
+                    return False
+        return False if path in excluded_paths else True
 
     def authorization_header(self, request=None) -> str:
-        # Placeholder logic for future implementation
-        return None
+        """ authorization header check"""
+        if request:
+            return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
-        # Placeholder logic for future implementation
+        """ current user method"""
         return None
