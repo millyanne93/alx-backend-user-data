c91da2813248dff71c705e827775316a821619e7
millyanne93 <nmillyanne20@gmail.com>
2024-06-05 08:06:18 +0000

    auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,16 +8,18 @@ from typing import List, TypeVar
 class Auth():
     """ manages the API authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ require authorithation check"""
+        """ require authorization check"""
         if path is None or excluded_paths is None or not len(excluded_paths):
             return True
         if path[-1] != '/':
             path += '/'
         for p in excluded_paths:
             if p.endswith('*'):
-                if path.startswith(p[:1]):
+                if path.startswith(p[:-1]):
                     return False
-        return False if path in excluded_paths else True
+            elif p == path:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ authorization header check"""
